b2a3fe385d4450abd8c7a82c730e69b328faac50
romanzhao <romanzhao@tencent.com>
2025-10-30 22:43:04 +0800

    refactor

--- core/strategy/trading/trading_strategy_common.py
+++ core/strategy/trading/trading_strategy_common.py
@@ -8,8 +8,8 @@ from common.logger import create_log
 from core.strategy.indicator.indicator_strategy_common import EnhancedVolumeIndicator
 import settings
 
-
 logger = create_log("trade_strategy_common")
+
 
 class TradeRecordManager:
     def __init__(self):
@@ -20,7 +20,6 @@ class TradeRecordManager:
 
     def transform_to_dataframe(self):
         return pd.DataFrame([record.__dict__ for record in self.trade_records])
-
 
 
 class TradeRecord:
@@ -64,7 +63,6 @@ class AssetRecord:
         self.total_assets = total_assets
 
 
-
 class EnhancedVolumeStrategy(bt.Strategy):
     """
     使用增强成交量指标的交易策略
@@ -75,11 +73,14 @@ class EnhancedVolumeStrategy(bt.Strategy):
         # 交易股票最小单位（股）
         ('min_order_size', settings.MIN_ORDER_SIZE if hasattr(settings, 'MIN_ORDER_SIZE') else 100),
         # 最大持仓比例 = 总持仓股票数量 * 持仓股票价格 / 总资产
-        ('max_portfolio_percent', settings.MAX_PORTFOLIO_PERCENT if hasattr(settings, 'MAX_PORTFOLIO_PERCENT') else 0.8),
+        ('max_portfolio_percent',
+         settings.MAX_PORTFOLIO_PERCENT if hasattr(settings, 'MAX_PORTFOLIO_PERCENT') else 0.8),
         # 单笔交易百分比（买） = 单笔交易费用（ 单笔交易股票价格 * 单笔交易量） / 总资产
-        ('max_single_buy_percent', settings.MAX_SINGLE_BUY_PERCENT if hasattr(settings, 'MAX_SINGLE_BUY_PERCENT') else 0.2),
+        ('max_single_buy_percent',
+         settings.MAX_SINGLE_BUY_PERCENT if hasattr(settings, 'MAX_SINGLE_BUY_PERCENT') else 0.2),
         # 单笔交易百分比（卖） = 单笔交易费用（ 单笔交易股票价格 * 单笔交易量） / 总资产
-        ('max_single_sell_percent', settings.MAX_SINGLE_SELL_PERCENT if hasattr(settings, 'MAX_SINGLE_SELL_PERCENT') else 0.3),
+        ('max_single_sell_percent',
+         settings.MAX_SINGLE_SELL_PERCENT if hasattr(settings, 'MAX_SINGLE_SELL_PERCENT') else 0.3),
 
     )
 
@@ -107,14 +108,14 @@ class EnhancedVolumeStrategy(bt.Strategy):
 
         # 执行交易信号
         if not np.isnan(self.indicator.lines.enhanced_buy_signal[0]):
-            logger.info(f'增强买入信号: {self.data.close[0]}')
+            logger.info(f'*** 增强买入信号 ***: {self.data.close[0]}')
             self.trading_strategy_buy()
             self.buy_signals_count += 1
         elif not np.isnan(self.indicator.lines.enhanced_sell_signal[0]):
-            logger.info(f'增强卖出信号: {self.data.close[0]}')
+            logger.info(f'*** 增强卖出信号 ***: {self.data.close[0]}')
             self.trading_strategy_sell()
             self.sell_signals_count += 1
-        # TODO DEBUG
+        # TODO 暂不执行普通的买入信号和普通卖出信号
         # elif not np.isnan(self.indicator.lines.main_buy_signal[0]):
         #     logger.info(f'主买入信号: {self.data.close[0]}')
         #     self.trading_strategy_buy()
@@ -150,18 +151,17 @@ class EnhancedVolumeStrategy(bt.Strategy):
                 # 确保购买股数为最小交易单位的整数倍
                 buy_size = buy_size // self.min_order_size * self.min_order_size
                 logger.info(
-                    f"资金管理: 可用资金={available_cash:.2f}, 总资产={total_asset_value:.2f}, 买入股数={buy_size}，买入价格={price:.2f}，买入后持仓={self.position.size+buy_size}")
+                    f"【买入挂单】: 可用资金={available_cash:.2f}, 总资产={total_asset_value:.2f}, 买入股数={buy_size}，理论买入价格={price:.2f}，买入后持仓={self.position.size + buy_size}")
                 # 计算并打印手续费
                 trade_commission = self.calculate_commission(buy_size, price)
                 if trade_commission:
-                    logger.info(f"交易手续费: {trade_commission['total_commission']:.2f}")
+                    logger.info(f"【理论交易手续费】: {trade_commission['total_commission']:.8f}")
                 self.order = self.buy(size=buy_size, price=price)
                 self.trade_record_manager.add_signal_record(self.data.datetime.date(0), 'B', 'strong_buy', buy_size)
             else:
                 logger.info(f"资金有限，预购买股数={buy_size}，小于最小交易单位={self.min_order_size}，无法购买")
         else:
-                logger.info(f"资金有限，可用资金={usable_cash:.2f}，成交最小金额={price * self.min_order_size:.2f}，无法购买")
-
+            logger.info(f"资金有限，可用资金={usable_cash:.2f}，成交最小金额={price * self.min_order_size:.2f}，无法购买")
 
     def trading_strategy_sell(self):
         # 有持仓时
@@ -172,49 +172,53 @@ class EnhancedVolumeStrategy(bt.Strategy):
             remaining_sell_size = current_position_size // self.min_order_size * self.min_order_size
             # 当前资产价值（包含当前持仓股票价值）
             total_asset_value = self.broker.getvalue()
+            # 可用资金（不包含当前持仓股票价值）
+            available_cash = self.broker.getcash()
             # 单次最大可卖出股数，确保卖出股数为最小交易单位的整数倍
             price = self.data.close[0]
             max_single_sell_size = total_asset_value * self.max_single_sell_percent / price // self.min_order_size * self.min_order_size
             # 确保卖出数量至少为最小交易单位
             sell_size = min(remaining_sell_size, max_single_sell_size)
             if sell_size >= self.min_order_size:
-                logger.info(f"卖出持仓: 当前持仓={self.position.size}, 实际卖出={sell_size}，卖出价格={price:.2f}，卖出后持仓={current_position_size - sell_size}")
+                logger.info(
+                    f"【卖出挂单】: 可用资金={available_cash:.2f}, 总资产={total_asset_value:.2f}, 当前持仓={self.position.size}, 卖出股数={sell_size}，理论卖出价格={price:.2f}，卖出后持仓={current_position_size - sell_size}")
                 # 计算并打印手续费
                 trade_commission = self.calculate_commission(sell_size, price)
                 if trade_commission:
-                    logger.info(f"交易手续费: {trade_commission['total_commission']:.2f}")
+                    logger.info(f"【理论交易手续费】: {trade_commission['total_commission']:.8f}")
                 self.order = self.sell(size=sell_size, price=price)
                 self.trade_record_manager.add_signal_record(self.data.datetime.date(0), 'S', 'strong_sell', sell_size)
             else:
-                logger.info(f"持仓有限，持仓股数={current_position_size}，预卖出股数={sell_size}，小于最小交易单位={self.min_order_size}，无法卖出")
+                logger.info(
+                    f"持仓有限，持仓股数={current_position_size}，预卖出股数={sell_size}，小于最小交易单位={self.min_order_size}，无法卖出")
         else:
             logger.info("当前无持仓，不执行卖出操作")
 
-    def log(self, txt, dt=None):
-        """记录交易日志"""
-        if self.p.print_log:
-            dt = dt or self.datas[0].datetime.date(0)
-            print(f'{dt.isoformat()} {txt}')
-
     def notify_order(self, order):
-        """订单状态通知"""
+        """订单状态通知，每笔订单状态改变都会触发"""
         if order.status in [order.Submitted, order.Accepted]:
             return
 
         if order.status in [order.Completed]:
             if order.isbuy():
-                logger.info(f'买入执行: 价格={order.executed.price:.2f}, 数量={order.executed.size}')
+                logger.info(
+                    f'【买入挂单成交】: 实际执行价格={order.executed.price:.2f}（含滑点）, 数量={order.executed.size}')
                 self.executed_buys_count += 1
             elif order.issell():
-                logger.info(f'卖出执行: 价格={order.executed.price:.2f}, 数量={order.executed.size}')
+                logger.info(
+                    f'【卖出挂单成交】: 实际执行价格={order.executed.price:.2f}（含滑点）, 数量={order.executed.size}')
                 self.executed_sells_count += 1
+            # 计算并记录实际佣金
+            actual_commission = self.calculate_commission(order.executed.size, order.executed.price)
+            logger.info(f"【实际交易手续费】: {actual_commission['total_commission']:.8f}")
+
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             logger.info('订单 取消/保证金不足/拒绝')
 
         self.order = None
 
     def notify_trade(self, trade):
-        """交易状态通知"""
+        """交易状态通知，平仓完成才会触发，部分平仓不会触发"""
         if not trade.isclosed:
             return
 
